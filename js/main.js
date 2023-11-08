b29b38ae15d4c3bc6f1086ef0a70c5edaedaf189
FrancescoMazzocchi <123@email.it>
2023-11-08 15:24:11 +0100

    feat: aggiunta bombe

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const start=document.getElementById("start");
 const container=document.getElementById("griglia");
 let celle;
+const nBombe=16;
 
 // CLICK START
 
@@ -12,13 +13,19 @@ start.addEventListener("click", function() {
     else if (difficolta=="dif2") {celle=81;}
     else if (difficolta=="dif3") {celle=49;}
     else {return;}
+    // genera bombe
+    const bombList = genBombe ();
+    console.log(bombList);
     // genera celle
-    generaGrid(container, "div", "quadrato", difficolta, celle)
+    generaGrid(container, "div", "quadrato", difficolta, celle, bombList)
+
+
 });
 
 // FUNZIONI
 
-function generaGrid (contenitore, elemento, classe1, classe2, celle) {
+// genera griglia
+function generaGrid (contenitore, elemento, classe1, classe2, celle, arrayBombe) {
     for (let i = 1; i <= celle; i++) {
         // crea elemento
         const square= document.createElement(elemento);
@@ -29,6 +36,10 @@ function generaGrid (contenitore, elemento, classe1, classe2, celle) {
         contenitore.append(square);
         // aggiunge scritta
         square.innerText = i;
+        // aggiunge bombe
+        if (arrayBombe.includes(i)) {
+            square.classList.add("bomb");
+        }
         // aggiunge evento click
         square.addEventListener("click", function() {
             console.log(i);
@@ -37,3 +48,19 @@ function generaGrid (contenitore, elemento, classe1, classe2, celle) {
     }
 }
 
+// genera intero casuale
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+//   genera bombe
+function genBombe () {
+    
+    let i=1
+    let bomba=[];
+    do {
+        bomba.push (getRndInteger(1, celle));
+        i++
+    } while (i <= nBombe)
+    return bomba;
+}
